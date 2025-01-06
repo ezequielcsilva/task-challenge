ad9a5431dc9d449b8ba476fa7975e8aebd448073
ezequielcsilva <falecomezequielcardoso@gmail.com>
2025-01-06 10:38:57 -0300

    feat: add task item entity

---
+++ src/TaskTracker.Domain/Abstractions/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskTracker.Domain.Abstractions;
+
+public abstract class Entity
+{
+    protected Entity(Guid id) => Id = id;
+
+    protected Entity()
+    {
+    }
+
+    public Guid Id { get; init; }
+}
---
+++ src/TaskTracker.Domain/Tasks/TaskItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using TaskTracker.Domain.Abstractions;
+
+namespace TaskTracker.Domain.Tasks;
+
+public class TaskItem : Entity
+{
+    public TaskItem(Guid id, string name, string description, bool isCompleted) : base(id)
+    {
+        Name = name;
+        Description = description;
+        IsCompleted = isCompleted;
+    }
+
+    private TaskItem()
+    { }
+
+    public string Name { get; private set; } = string.Empty;
+    public string Description { get; private set; } = string.Empty;
+    public bool IsCompleted { get; private set; }
+
+    public static TaskItem Create(string name, string description, bool isCompleted)
+    {
+        return new TaskItem(Guid.NewGuid(), name, description, isCompleted);
+    }
+
+
+}
